21c5276a69c98a12809bfc3c78e286902d0009f1
michael-greene-ey <michael.greene2@ey.com>
2024-04-10 17:22:10 -0500

    III commit

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
